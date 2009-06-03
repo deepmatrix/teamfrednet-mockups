3e5e441fc58f31668ab7eaa35bdae7bc83d62180
john.pritchard <john.pritchard@teamfrednet.org>
2009-06-03 22:40:17 +0000

    reorg

--- vrml/PicoRover/pack.sh
+++ vrml/PicoRover/pack.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+
 if [ "$1" ]&&[ -d bin ]
 then
  version="$1"
+ targets=$(2>/dev/null ls PicoRover-*.jar )
+ if [ -n "${targets}" ]
+ then
+  if ! rm -f ${targets}
+  then
+   cat<<EOF>&2
+Error dropping stale targets. 
+EOF
+   exit 1
+  fi
+ fi
  cd bin
  jar cvmf ../mf.txt ../PicoRover-${version}.jar $(find . -type f | egrep -v '\.svn')
 else
- cat<<EOF
+ cat<<EOF>&2
 usage
   ./pack.sh version
 example
