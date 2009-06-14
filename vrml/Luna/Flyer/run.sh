7460c4150da8d975142ba152ba45b22fd0a3df70
john.pritchard <john.pritchard@teamfrednet.org>
2009-06-14 05:27:56 +0000

    fix

--- vrml/Luna/Flyer/run.sh
+++ vrml/Luna/Flyer/run.sh
@@ -10,9 +10,8 @@ fi
 lib_path=${wd}/lib/${name}/${mach}
 if [ -d "${lib_path}" ]
 then
- echo "Library directory '${lib_path}'."
- java -Djava.library.path=${lib_path} -cp $(./classpath.sh) de.jreality.vr.ViewerVR ${wd}/Luna.wrl
+ java -Djava.library.path=${lib_path} -cp $(${wd}/classpath.sh) de.jreality.vr.ViewerVR ${wd}/Luna.wrl
 else
- echo "Library directory not found '${lib_path}'."
+ echo "Application library directory for OS '${name}' and ARCH '${mach}' not found: '${lib_path}'."
  exit 1
 fi
