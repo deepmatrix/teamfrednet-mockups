fb3b2ddeef5aaa829cce946a7e9c70e0e8acc6f9
john.pritchard@teamfrednet.org <john.pritchard@teamfrednet.org@938af900-4f22-11de-aa6a-4703752c21e7>
2009-06-19 09:04:33 +0000

    bugz

--- lro/comms/copy/test/test.js
+++ lro/comms/copy/test/test.js
@@ -1,4 +1,32 @@
 
+var hexchars = "012345678abcdef";
+
+function dehex8(x){
+
+    var bytes = new Array();
+    for (var cc = 0, count = x.length(); cc < count; ){
+        var c0 = x.charAt(cc++);
+        if (cc < count){
+            var c1 = x.charAt(cc++);
+            var b = (hexchars.indexOf(c0)<<4)+(hexchars.indexOf(c0));
+            bytes.push(b);
+        }
+        else {
+            var b = hexchars.indexOf(c0);
+            bytes.push(b);
+        }
+    }
+    return bytes;
+}
+function dehex32(x){
+    var words = new Array();
+    var bytes = dehex8(x);
+    for (var cc = 0, count = (bytes.length * 8); cc < count; cc += 8){
+        words[cc>>5] |= (bytes[cc / 8] & 0xff) << (24-(cc%32));
+    }
+    return words;
+}
+
 var line;
 while (line = readline()){
     var vector = line.split(':');
