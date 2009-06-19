b80f734fc22ea1e14973bac30fc7c80e80f71d5c
john.pritchard@teamfrednet.org <john.pritchard@teamfrednet.org@938af900-4f22-11de-aa6a-4703752c21e7>
2009-06-19 22:56:28 +0000

    fix

--- dtn/ctdp/SDNV.java
+++ dtn/ctdp/SDNV.java
@@ -11,16 +11,49 @@ public class SDNV
     public final static int Read(InputStream in)
         throws IOException
     {
-        int value = 0;
+        int value = 0, u7, u7a = 0, u7b = 0, u7c = 0;
         for (int cc = 3; ; cc--){
             int u8 = in.read();
             if (-1 == u8)
                 throw new EOFException();
             else {
-                int u7 = (u8 & 0x7f);
-                value |= (u7 << (cc*7));
-                if (0 == cc || u7 == u8)
+                u7 = (u8 & 0x7f);
+                switch (cc){
+                case 3:
+                    if (u7 == u8)
+                        return u7;
+                    else {
+                        u7a = u7;
+                        break;
+                    }
+                case 2:
+                    if (u7 == u8){
+                        value |= (u7a << 7);
+                        value |= u7;
+                        return value;
+                    }
+                    else {
+                        u7b = u7;
+                        break;
+                    }
+                case 1:
+                    if (u7 == u8){
+                        value |= (u7a << 14);
+                        value |= (u7b << 7);
+                        value |= (u7);
+                        return value;
+                    }
+                    else {
+                        u7c = u7;
+                        break;
+                    }
+                case 0:
+                    value |= (u7a << 21);
+                    value |= (u7b << 14);
+                    value |= (u7c << 7);
+                    value |= (u7);
                     return value;
+                }
             }
         }
     }
